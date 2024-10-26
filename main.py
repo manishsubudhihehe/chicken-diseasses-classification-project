6978ac98979f33d348cdf873aa91b76a5d90d697
manish subudhi <manishsubudhi789@gmail.com>
2024-10-26 23:01:28 +0530

    prepare base model completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipelinepython
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -10,3 +11,5 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
